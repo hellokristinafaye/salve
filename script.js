7f1c5772d0609d0af92af73672ba9086812cb3eb
Kristina Faye <hellokristinafaye@gmail.com>
2025-04-29 17:21:44 -0700

    getComputerChoice function works and logs nicely in the console

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const humanScoreText = document.getElementById("human-score");
 const computerScoreText = document.getElementById("computer-score");
+const hand = ['rock', 'paper', 'scissors'];
+let humanChoice;
+let computerChoice;
 
-let humanScore = 8;
-let computerScore = 9;
+let humanScore = 0;
+let computerScore = 0;
 
 humanScoreText.innerHTML = humanScore
 computerScoreText.innerHTML = computerScore
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * hand.length);
+    computerChoice = hand[choice];
+    console.log(`Computer: ${computerChoice}`);
+    return computerChoice;
+}
+getComputerChoice()
