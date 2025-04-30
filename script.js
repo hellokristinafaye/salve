1849baa9e025fd68f5a626d970647a43752eee8d
Kristina Faye <hellokristinafaye@gmail.com>
2025-04-29 17:15:36 -0700

    successful dynamic span update

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("infused honeys")
+const humanScoreText = document.getElementById("human-score");
+const computerScoreText = document.getElementById("computer-score");
+
+let humanScore = 8;
+let computerScore = 9;
+
+humanScoreText.innerHTML = humanScore
+computerScoreText.innerHTML = computerScore
