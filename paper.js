8cf56bfb281f07738dde3c89453efdba626affdd
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-01 15:51:55 -0700

    draws score increments and displays!

--- paper.js
+++ paper.js
@@ -5,6 +5,8 @@ let humanScore = 0;
 let computerScore = 0;
 let drawScore = 0;
 
+const drawScoreText = document.getElementById("draw-score");
+
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
@@ -19,7 +21,20 @@ console.log(`Human: ${humanChoice}`)
 
 function playRound(humanChoice) {
     let lowerCaseHumanChoice = humanChoice.toLowerCase();
-    console.log(lowerCaseHumanChoice)
+    console.log(`lowerCaseHumanChoice: ${lowerCaseHumanChoice}`)
+
+    if (lowerCaseHumanChoice === computerChoice) {
+        console.log("Draw!")
+        drawScore++
+        console.log(drawScore)
+        drawScoreText.innerHTML = drawScore
+
+    } else if (
+        lowerCaseHumanChoice === "rock" &&computerChoice === "paper"
+    ) {
+
+    }
+
 
 }
 playRound(humanChoice);
