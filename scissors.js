b2d412fb51ba3bd11c5a5bacdf37d978451944a4
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-05 13:46:07 -0700

    prints properly

--- scissors.js
+++ scissors.js
@@ -3,6 +3,14 @@ console.log("snip snip")
 function getComputerChoice() {
     let num = Math.round(Math.random() * 2)
 
+    if (num === 0) {
+        console.log("rock");
+    } else if (num === 1) {
+        console.log("paper");
+    } else if (num === 2) {
+        console.log("scissors");
+    }
+
     console.log(num);
 }
 
